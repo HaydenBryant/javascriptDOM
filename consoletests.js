89776ab7d42a1b7f29d6d2e06cd6eec71f08c359
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-04-03 17:25:18 +0000

    lesson 3

--- consoletests.js
+++ consoletests.js
@@ -1,2 +1,9 @@
 var banner = document.getElementById('page-banner');
 var bookList = document.getElementById('book-list');
+
+var titles = document.getElementsByClassName('title');
+titles[0];
+var lis = document.getElementsByTagName('li');
+for (i = 0; i < titles.length; i++) {
+    console.log(titles[i]);
+}
