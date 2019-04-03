120a51b924c2efc3f190dd8e8fa7ed4a880f34f8
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-04-03 21:36:19 +0000

    lesson 6

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-// console.log(wmf)
+const banner = document.querySelector('#page-banner');
 
-var books = document.querySelector('#book-list li .name');
+console.log('#page-banner node type is:',banner.nodeType);
+console.log('#page-banner name type is:',banner.nodeName);
+console.log('#page-banner haschild nodes:',banner.hasChildNodes());
 
-books = document.querySelectorAll('#book-list li .name');
-
-
-Array.from(books).forEach(function(book) {
-    console.log(book);
-});
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
